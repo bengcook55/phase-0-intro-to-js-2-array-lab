f0d7ad92d87057aac2ffb8a7d3f4f335ca5063b9
Benjamin Cook <bengcook55@gmail.com>
2022-11-11 20:38:50 -0500

    commit tree 48e4889621a5643fbcd5a871b20a088bd7a274c3

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+    return cats
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+    return cats
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats
+    
+}
+
+function appendCat(name){
+    const newCats = [...cats, name];
+    return newCats
+}
+
+function prependCat(name){
+    const moreCats = [name, ...cats]
+    return moreCats
+}
+
+function removeLastCat(){
+    const newCats = cats.slice(0, -1);
+    return newCats
+}
+
+function removeFirstCat(){
+    const newCats = cats.slice(1)
+    return newCats
+}
